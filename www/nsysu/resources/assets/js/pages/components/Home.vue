8cb7ad9299ea2bc53512a4e62d137ea3f1dad973
jerry <jerry.liu@touchcity.tw>
2020-08-31 15:42:09 +0800

    add tiff

--- www/nsysu/resources/assets/js/pages/components/Home.vue
+++ www/nsysu/resources/assets/js/pages/components/Home.vue
@@ -38,6 +38,7 @@ export default {
   },
   async mounted() {
     var vm=this;
+    await vm.getData();
     // const url =siteUrl+'/tiff/test.tif';
     var mymap = L.map('map').setView([23, 120], 5);;
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -48,26 +49,37 @@ export default {
        zoomOffset: -1,
        accessToken: 'pk.eyJ1IjoiYXBwbGU3OTEyMTAiLCJhIjoiY2s2aGp1Zjl6MHZveTNrbXRld2ViZzdtcSJ9.P9qkKbnkCZPCqGDXHbr2aA'
     }).addTo(mymap);
-    const windSpeedUrl=siteUrl+'/tiff/testTFW.tif'
+    const windSpeedUrl=siteUrl+'/tiff/point2.tiff'
     // https://stuartmatthews.github.io/leaflet-geotiff/tif/wind_speed.tif';
     const plottyRenderer = L.LeafletGeotiff.plotty({
-        displayMin: 0,
-        displayMax: 10,
-        clampLow: false,
-        clampHigh: false,
+        displayMin: 0.01,
+        displayMax: 1,
+        clampLow: true,
+        clampHigh: true,
+        colorScale:"summer"
+        
       });
-    const windSpeedLayer = L.leafletGeotiff(windSpeedUrl, {
-        band: 0,
-        renderer: plottyRenderer,
-      }).addTo(mymap);
-      const windSpeedUrl2=siteUrl+'/tiff/wind_speed.tif'
-      const windSpeedLayer2 = L.leafletGeotiff(windSpeedUrl2, {
-        band: 0,
-        renderer: plottyRenderer,
-      }).addTo(mymap);
+    // const windSpeedLayer = L.leafletGeotiff(windSpeedUrl, {
+    //     band: 0,
+    //     renderer: plottyRenderer,
+    //     opacity: 0.1,
+    //   }).addTo(mymap);
+      // const windSpeedUrl2=siteUrl+'/tiff/wind_speed.tif'
+      // const windSpeedLayer2 = L.leafletGeotiff(windSpeedUrl2, {
+      //   band: 0,
+      //   renderer: plottyRenderer,
+      // }).addTo(mymap);
 
 
-    // await vm.getData();
+//     viridis	inferno	turbo
+// rainbow	jet	hsv
+// hot	cool	spring
+// summer	autumn	winter
+// bone	copper	greys
+// yignbu	greens	yiorrd
+// bluered	rdbu	picnic
+// portland	blackbody	earth
+// electric	magma	plasma
     console.log('this');
     console.log(vm.data);
     
@@ -76,22 +88,22 @@ export default {
 
     
     
-    // let velocity = L.velocityLayer({
-    //   displayValues: true,
-    //   displayOptions: {
-    //     velocityType: 'GBR Wind',
-    //     position: 'bottomleft',//REQUIRED !
-    //     emptyString: 'No velocity data', //REQUIRED !
-    //     angleConvention: 'bearingCW',
-    //     displayPosition: 'bottomleft',
-    //     displayEmptyString: 'No velocity data',
-    //     speedUnit: 'm/s'
-    //   },
-    //   data: vm.data,
-    //   maxVelocity: 10,
-    //   velocityScale: 0.05,
-    // });
-    // velocity.addTo(mymap);
+    let velocity = L.velocityLayer({
+      displayValues: true,
+      displayOptions: {
+        velocityType: 'GBR Wind',
+        position: 'bottomleft',//REQUIRED !
+        emptyString: 'No velocity data', //REQUIRED !
+        angleConvention: 'bearingCW',
+        displayPosition: 'bottomleft',
+        displayEmptyString: 'No velocity data',
+        speedUnit: 'm/s'
+      },
+      data: vm.data,
+      maxVelocity: 10,
+      velocityScale: 0.05,
+    });
+    velocity.addTo(mymap);
 
   },
   created() {
